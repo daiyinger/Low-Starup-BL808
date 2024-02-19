b7f59d814b3550a2a859ea94d8a45a62ae2f9c03
daiyinger <1002639334@qq.com>
2024-02-19 05:35:11 -0800

    support load linux

--- spl/spl-project/d0_spl/main.c
+++ spl/spl-project/d0_spl/main.c
@@ -3,20 +3,84 @@
 #include "bl808_glb.h"
 #include "bflb_flash.h"
 #include "bflb_l1c.h"
+#include "rv_pmp.h"
 
 #define     D0_FLASH_BASIC_ADDR         0x100000
 
 #define     DTB_SRC_ADDR                0x110000
-#define     DTB_DST_ADDR                0x50010000
+#define     DTB_DST_ADDR 		0x51ff8000
 
 #define     OPENSBI_SRC_ADDR            0x120000
-#define     OPENSBI_DST_ADDR            0x50000000
+#define     OPENSBI_DST_ADDR		0x53F00000
 
 #define     KERNEL_SRC_ADDR             0x130000
-#define     KERNEL_DST_ADDR             0x50100000
+#define     KERNEL_DST_ADDR 		0x50000000
+
+#define     ROOTFS_SRC_ADDR 		0x580000
+#define     ROOTFS_DST_ADDR 		0x52000000
 
 #define     COMPRESS_FLAG_SIZE          1
 #define     KERNEL_LEN_SIZE             4
+
+#if (__riscv_xlen == 64)
+/* linux pmp setting */
+const pmp_config_entry_t pmp_entry_tab[9] = {
+    [0] = {
+        .entry_flag = ENTRY_FLAG_ADDR_NAPOT | ENTRY_FLAG_PERM_W | ENTRY_FLAG_PERM_R,
+        .entry_pa_base = 0x20000000,
+        .entry_pa_length = 0x10000,
+    },
+
+    [1] = {
+        .entry_flag = ENTRY_FLAG_ADDR_NAPOT | ENTRY_FLAG_PERM_W | ENTRY_FLAG_PERM_R,
+        .entry_pa_base = 0x30000000,
+        .entry_pa_length = PMP_REG_SZ_1M,
+    },
+
+    [2] = {
+        .entry_flag = ENTRY_FLAG_ADDR_NAPOT | ENTRY_FLAG_PERM_X | ENTRY_FLAG_PERM_W | ENTRY_FLAG_PERM_R,
+        .entry_pa_base = 0x3eff0000,
+        .entry_pa_length = 0x10000,
+    },
+
+    [3] = {
+        .entry_flag = ENTRY_FLAG_ADDR_NAPOT | ENTRY_FLAG_PERM_X | ENTRY_FLAG_PERM_W | ENTRY_FLAG_PERM_R,
+        .entry_pa_base = 0x40000000,
+        .entry_pa_length = PMP_REG_SZ_16K,
+    },
+    
+    [4] = {
+        .entry_flag = ENTRY_FLAG_ADDR_NAPOT | ENTRY_FLAG_PERM_X | ENTRY_FLAG_PERM_W | ENTRY_FLAG_PERM_R,
+        .entry_pa_base = 0x50000000,
+        .entry_pa_length = 0x4000000,
+    },
+
+    [5] = {
+        .entry_flag = ENTRY_FLAG_ADDR_NAPOT | ENTRY_FLAG_PERM_W | ENTRY_FLAG_PERM_R,
+        .entry_pa_base = 0x58000000,
+        .entry_pa_length = 0x4000000,
+    },
+
+    [6] = {
+        .entry_flag = ENTRY_FLAG_ADDR_NAPOT | ENTRY_FLAG_PERM_W | ENTRY_FLAG_PERM_R,
+        .entry_pa_base = 0xe0000000,
+        .entry_pa_length = 0x8000000,
+    },
+    
+    [7] = {
+        .entry_flag = ENTRY_FLAG_ADDR_NAPOT | ENTRY_FLAG_PERM_X | ENTRY_FLAG_PERM_W | ENTRY_FLAG_PERM_R,
+        .entry_pa_base = 0x3F000000,
+        .entry_pa_length = 0x10000,
+    },
+
+    [8] = {
+        .entry_flag = ENTRY_FLAG_ADDR_TOR,
+        .entry_pa_base = 0xffffffffff, /* 40-bit PA */
+        .entry_pa_length = 0,
+    },
+};
+
+#endif
 
 static int load_image(void)
 {
@@ -25,9 +89,11 @@ static int load_image(void)
 
 
     uint32_t header_kernel_len = 0;
+    uint32_t rootfs_len = 0;
     uint8_t kernel[KERNEL_LEN_SIZE];
     uint8_t compress_flag;
 
+    //printf("load start\r\n");
     flash_addr = KERNEL_SRC_ADDR - (COMPRESS_FLAG_SIZE + KERNEL_LEN_SIZE) - D0_FLASH_BASIC_ADDR;
     read_buf = (uint8_t *)(FLASH_XIP_BASE + flash_addr);
     bflb_l1c_dcache_invalidate_range(read_buf, COMPRESS_FLAG_SIZE + KERNEL_LEN_SIZE);
@@ -55,7 +121,7 @@ static int load_image(void)
             read_buf = (uint8_t *)(FLASH_XIP_BASE + flash_addr);
             bflb_l1c_dcache_invalidate_range(read_buf, header_kernel_len);
             unlz4((const void *)read_buf, (void *)KERNEL_DST_ADDR, header_kernel_len);
-        }
+	}
         else 
         {
             flash_addr = KERNEL_SRC_ADDR - D0_FLASH_BASIC_ADDR;
@@ -64,12 +130,26 @@ static int load_image(void)
             memcpy((void *)KERNEL_DST_ADDR, read_buf, header_kernel_len);
         }
     }
+    
 
     //copy dtb code
     flash_addr = DTB_SRC_ADDR - D0_FLASH_BASIC_ADDR;
     read_buf = (uint8_t *)(FLASH_XIP_BASE + flash_addr);
     bflb_l1c_dcache_invalidate_range(read_buf, 0x10000);
     memcpy((void *)DTB_DST_ADDR, read_buf, 0x10000);
+
+
+    flash_addr = ROOTFS_SRC_ADDR - D0_FLASH_BASIC_ADDR - 0x04;
+    read_buf = (uint8_t *)(FLASH_XIP_BASE + flash_addr);
+    bflb_l1c_dcache_invalidate_range(read_buf, 4);
+    rootfs_len = *((uint32_t *)read_buf);
+
+
+    flash_addr = ROOTFS_SRC_ADDR - D0_FLASH_BASIC_ADDR;
+    read_buf = (uint8_t *)(FLASH_XIP_BASE + flash_addr);
+    bflb_l1c_dcache_invalidate_range(read_buf, rootfs_len);
+    unlz4((const void *)read_buf, (void *)ROOTFS_DST_ADDR, rootfs_len);
+
 
     /* Copy opensbi code */
     flash_addr = OPENSBI_SRC_ADDR - D0_FLASH_BASIC_ADDR;
@@ -94,11 +174,15 @@ int main(void)
             bflb_mtimer_delay_ms(1000);
     }
 
+
     __ASM volatile("csrw mcor, %0"
                    :
                    : "r"(0x30013));
 
+    rvpmp_init(pmp_entry_tab, sizeof(pmp_entry_tab) / sizeof(pmp_config_entry_t));
+    
     __ISB();
+
 
     void (*opensbi)(int hart_id, int fdt_addr) = (void (*)(int hart_id, int fdt_addr))OPENSBI_DST_ADDR;
     
